07a437fcd67af8f7b53318099062ae90a52e1694
ZAIRA ICELA SILVA SANCHEZ <138176414+iceladeveloper@users.noreply.github.com>
2024-02-01 10:12:24 -0600

    my first commit

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public void Start() {
+        // Imprime el mensaje "Hola mundo" en la consola
+        Debug.Log("Hola mundo!");
+    }
+}
+
+
